2ad2b6204e1296f5b0f344e0e53d0e503488d47b
Teale Foster <tealefoster@gmail.com>
2020-10-06 15:09:14 -0500

    Build handleClick function

--- static/js/app.js
+++ static/js/app.js
@@ -12,7 +12,7 @@ function buildTable(data) {
     data.forEach((dataRow) => {
         // Append a row to the table body
         let row = tbody.append("tr");
-        
+
         // Loop through each field in the dataRow and add
         // each value as a table cell (td)
         Object.values(dataRow).forEach((val) => {
@@ -23,3 +23,27 @@ function buildTable(data) {
     });
 }
 
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+        // Apply `filter` to the table data to only keep the
+        // rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+};
+
+// Identify when the filter button is clicked and execute handleClick func
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
